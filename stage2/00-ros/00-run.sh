8b9de79350b4a7cf5eb0086f2139d5d1ab7f069f
Błażej Sowa <bsowa123@gmail.com>
2022-05-05 15:18:03 +0200

    Install ROS2 foxy

--- stage2/00-ros/00-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-install -v -m 644 files/ros-latest.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
-
-on_chroot << EOF
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
-apt-get update
-EOF
