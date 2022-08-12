b1d81388efd97c2581617f5218759740da1627f3
Błażej Sowa <bsowa123@gmail.com>
2022-08-12 12:22:00 +0000

    Disable all ubuntu advantage systemd units

--- stage3/06-update-motd/01-run.sh
+++ stage3/06-update-motd/01-run.sh
@@ -4,4 +4,10 @@ rm -rf "${ROOTFS_DIR}/etc/update-motd.d/"*
 
 install -v -m 755 files/* "${ROOTFS_DIR}/etc/update-motd.d/"
 
-on_chroot "systemctl disable ua-messaging.timer"
+on_chroot <<EOF
+systemctl disable ua-timer.timer
+systemctl disable update-notifier-download.timer
+systemctl disable update-notifier-motd.timer
+systemctl disable ua-reboot-cmds.service
+systemctl disable ubuntu-advantage.service
+EOF
