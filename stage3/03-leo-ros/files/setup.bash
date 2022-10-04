552bb6b4ce4e460b86c7599008049f3569d3942f
Błażej Sowa <bsowa123@gmail.com>
2022-10-04 16:03:53 +0200

    Force colored logs for ros2 launch

--- stage3/03-leo-ros/files/setup.bash
+++ stage3/03-leo-ros/files/setup.bash
@@ -10,5 +10,7 @@ LAUNCH_ARGS=""
 UROS_AGENT_ARGS="serial -D /dev/serial0 -b 460800"
 
 ### ROS Environment Variables
-#export ROS_DOMAIN_ID
+#export ROS_DOMAIN_ID=10
 #export ROS_NAMESPACE="leo1"
+#export ROS_LOCALHOST_ONLY=1
+export RCUTILS_COLORIZED_OUTPUT=1
