fe04603d71e549b0b956ceadfe2db24a7e32d173
Błażej Sowa <bsowa123@gmail.com>
2022-10-05 19:03:00 +0200

    Update LXQt configuration

--- stage4/00-desktop-environment/01-run.sh
+++ stage4/00-desktop-environment/01-run.sh
@@ -8,3 +8,8 @@ install -v -m 644 files/nm-tray/nm-tray.conf "${ROOTFS_DIR}/etc/xdg/nm-tray/"
 
 sed -i "s|user = CHANGE_ME|user = ${FIRST_USER_NAME}|" "${ROOTFS_DIR}/etc/lightdm/lightdm-mini-greeter.conf"
 install -v -m 644 files/lightdm/greeter.conf "${ROOTFS_DIR}/etc/lightdm/lightdm.conf.d/"
+
+install -v -m 644 files/lxqt/* "${ROOTFS_DIR}/etc/xdg/lxqt/"
+
+mkdir -v -p "${ROOTFS_DIR}/etc/xdg/pcmanfm-qt/lxqt"
+install -v -m 644 files/pcmanfm-qt/settings.conf "${ROOTFS_DIR}/etc/xdg/pcmanfm-qt/lxqt/"
