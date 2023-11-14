ba71e786ee38ae04b641168c072faa7de156f0e4
Błażej Sowa <bsowa123@gmail.com>
2023-11-14 16:28:41 +0100

    Simplify motd, don't install Ubuntu advantage

--- stage3/06-update-motd/01-run.sh
+++ stage3/06-update-motd/01-run.sh
@@ -3,11 +3,3 @@
 rm -rf "${ROOTFS_DIR}/etc/update-motd.d/"*
 
 install -v -m 755 files/* "${ROOTFS_DIR}/etc/update-motd.d/"
-
-on_chroot <<EOF
-systemctl disable ua-timer.timer
-systemctl disable update-notifier-download.timer
-systemctl disable update-notifier-motd.timer
-systemctl disable ua-reboot-cmds.service
-systemctl disable ubuntu-advantage.service
-EOF
